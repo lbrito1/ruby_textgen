3f79d20cc193fc7139217b8dba7697aca25054ec
Leonardo <lbrito@gmail.com>
2015-09-02 18:43:21 -0300

    Using method_missing to improve parsing
    By using method_missing we can improve our script's readability and refactor some important code.

--- lerolero.rb
+++ lerolero.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-#
 require_relative 'grammar'
 require_relative 'dictionary'
 require_relative 'rule'
@@ -7,20 +6,33 @@ require_relative 'codex'
 
 @grammar = Grammar.new
 
-def dictionary key, *values
-  puts "Read dictionary with: #{key} #{values.to_s}"
-  Dictionary.instance.add key, values
+def dictionary
+  @state = :dictionary
 end
 
-def rule name, *keys
-  puts "Read rule with: #{name} #{keys.to_s}"
-  @grammar.rules[name] = Rule.new keys
+def rule
+  @state = :rule
 end
 
 def codex *rulenames
   puts "Read codex with: #{rulenames.to_s}"
   @grammar.codex << (Codex.new rulenames)
 end
+
+def method_missing method, *args, &block
+  @state ||= :idle
+  case @state
+  when :dictionary
+    puts "Read dictionary with: #{method.to_s} #{args.to_s}"
+    Dictionary.instance.add method.to_s, args
+  when :rule
+    puts "Read rule with: #{method.to_s} #{args.to_s}"
+    @grammar.rules[method.to_s] = (Rule.new args)
+  when :idle
+  else
+    puts "Boom! Something went wrong. I don't know what to do with #{@state.to_s}."
+  end
+end  
 
 load 'script.lero'
 @grammar.generate
